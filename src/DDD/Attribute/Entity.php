4e7d5e9b4dfc85674cefbd7bdd4882fb0839e86d
Henning Schwentner <hs@wps.de>
2021-05-11 16:40:48 +0200

    Add doc comments

--- src/DDD/Attribute/Entity.php
+++ src/DDD/Attribute/Entity.php
@@ -6,7 +6,20 @@ namespace PHPMolecules\DDD\Attribute;
 
 use Attribute;
 
-#[Attribute]
+/**
+ * Identifies an {@link Entity}. Entities represent a thread of continuity and identity, going through a lifecycle,
+ * though their attributes may change. Means of identification may come from the outside, or it may be an arbitrary
+ * identifier created by and for the system, but it must correspond to the identity distinctions in the model. The model
+ * must define what it means to be the same thing.
+ *
+ * @author Christian Stettler
+ * @author Henning Schwentner
+ * @author Stephan Pirnbaum
+ * @author Martin Schimak
+ * @author Oliver Drotbohm
+ * @link https://domainlanguage.com/wp-content/uploads/2016/05/DDD_Reference_2015-03.pdf Domain-Driven Design Reference (Evans) - Entities
+ */
+#[Attribute(Attribute::TARGET_CLASS)]
 class Entity
 {
 }
--- src/DDD/Attribute/ValueObject.php
+++ src/DDD/Attribute/ValueObject.php
@@ -6,7 +6,19 @@ namespace PHPMolecules\DDD\Attribute;
 
 use Attribute;
 
-#[Attribute]
+/**
+ * Identifies a value object. Domain concepts that are modeled as value objects have no conceptual identity or
+ * lifecycle. Implementations should be immutable, operations on it are side-effect free.
+ *
+ * @author Christian Stettler
+ * @author Henning Schwentner
+ * @author Stephan Pirnbaum
+ * @author Martin Schimak
+ * @author Oliver Drotbohm
+ * @see <a href="https://domainlanguage.com/wp-content/uploads/2016/05/DDD_Reference_2015-03.pdf">Domain-Driven Design
+ *      Reference (Evans) - Value objects</a>
+ */
+#[Attribute(Attribute::TARGET_CLASS)]
 class ValueObject
 {
 }
